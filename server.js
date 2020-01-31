9c083af3e7e1d641bf5d024094cb90d5ff9e215b
Bo <deleon.reescano@ricealumni.net>
2020-01-31 13:35:53 -0600

    configured user registration and authentication routes

--- server.js
+++ server.js
@@ -10,4 +10,13 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
+// app.get("/", (req, res) => res.send("API Running"));
+
+// Define Routes. Requires routers to handle any kind of
+// HTTP request @ /api/users as long as that type of router
+// exists in users.js (for get request on /api/user/thispage,
+// there must be a corresponding router at /thispage)
+app.use("/api/users", require("./routes/api/users"));
+
+// Define Routes. ""
+app.use("/api/auth", require("./routes/api/auth"));
