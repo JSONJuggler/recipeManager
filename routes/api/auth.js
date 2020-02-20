87bbd8a19d0a9631cf81a186e409c3db6773b37f
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-02-20 17:01:52 -0600

    Hide mongoURI and jwtSECRET in environment variables

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const config = require("config");
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
 
@@ -64,7 +63,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        process.env.JWTSECRET,
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,6 @@ const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const config = require("config");
 const User = require("../../models/User");
 
 const router = express.Router();
@@ -60,7 +59,7 @@ router.post(
       };
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        process.env.JWTSECRET,
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
