54a3af33796b6ac7366ff2c827c5d1dc0e2d3339
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-03-05 12:43:19 -0600

    baselines set

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import { CssBaseline } from "@material-ui/core";
+
 import rootReducer from "./reducers";
 
 const initialState = {};
@@ -25,6 +27,7 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
+    <CssBaseline />
     <App />
   </Provider>,
   document.getElementById("root")
