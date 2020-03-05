0ed716896edf4d466822453177e44faeca82b405
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-03-05 15:15:54 -0600

    log in page customization

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,140 +1,10 @@
-// import React, { Fragment, useState, useEffect } from "react";
-// import { Redirect } from "react-router-dom";
-// import { connect } from "react-redux";
-// import PropTypes from "prop-types";
-
-// import { Button } from "@material-ui/core";
-// import validate from "./loginValidate";
-// import { login } from "../../actions/auth";
-// import { Styledsub, Styledinput, Styledlink, Darkbox } from "../../stylings";
-
-// const Login = ({ isAuthenticated, login }) => {
-//   const [formData, setFormData] = useState({
-//     email: "",
-//     password: ""
-//   });
-
-//   const [errors, setErrors] = useState({});
-
-//   const [isSubmitting, setIsSubmitting] = useState(false);
-
-//   const { email, password } = formData;
-
-//   const onChange = e => {
-//     setFormData({ ...formData, [e.target.name]: e.target.value });
-//     isSubmitting && setErrors(validate(formData));
-//   };
-
-//   const onSubmit = async e => {
-//     if (e) {
-//       e.preventDefault();
-//       setErrors(validate(formData));
-//       setIsSubmitting(true);
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (Object.keys(errors).length === 0 && isSubmitting) {
-//       setIsSubmitting(false);
-//       login({ email, password });
-//     }
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [errors]);
-
-//   useEffect(() => {
-//     if (isSubmitting) {
-//       setIsSubmitting(false);
-//       window.scrollTo(0, 0);
-//     }
-//   }, [isSubmitting]);
-
-//   if (isAuthenticated) {
-//     return <Redirect to="/dashboard" />;
-//   }
-
-//   return (
-//     <Fragment>
-//       <Darkbox>
-//         <h1>Login to access your personal recipes!</h1>
-//         <form>
-//           <div>
-//             <Styledinput
-//               validated={errors.email}
-//               type="text"
-//               name="email"
-//               placeholder="Email"
-//               value={email}
-//               onChange={e => onChange(e)}
-//             />
-//           </div>
-//           {errors.email && (
-//             <p
-//               style={{
-//                 fontSize: ".7rem",
-//                 float: "left",
-//                 margin: "0px",
-//                 padding: "0px"
-//               }}
-//             >
-//               {errors.email}
-//             </p>
-//           )}
-//           <div>
-//             <Styledinput
-//               validated={errors.password}
-//               type="password"
-//               name="password"
-//               placeholder="Password"
-//               value={password}
-//               onChange={e => onChange(e)}
-//             />
-//             {errors.password && (
-//               <p
-//                 style={{
-//                   fontSize: ".7rem",
-//                   float: "left",
-//                   margin: "0px",
-//                   padding: "0px"
-//                 }}
-//               >
-//                 {errors.password}
-//               </p>
-//             )}
-//           </div>
-
-//           {/* <Styledsub type="submit" value="Login" /> */}
-//         </form>
-//         <Button variant="contained" onClick={e => onSubmit(e)}>
-//           Login
-//         </Button>
-//         <div>
-//           <Styledlink to="/Register">
-//             Don't have an account? Register here!
-//           </Styledlink>
-//         </div>
-//       </Darkbox>
-//     </Fragment>
-//   );
-// };
-
-// Login.propTypes = {
-//   isAuthenticated: PropTypes.bool,
-//   login: PropTypes.func.isRequired
-// };
-
-// const mapStateToProps = state => ({
-//   isAuthenticated: state.auth.isAuthenticated
-// });
-
-// export default connect(mapStateToProps, { login })(Login);
-
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
@@ -142,14 +12,22 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import { Redirect, Link as rrLink } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+// import { useTheme } from "@material-ui/core/styles";
+
+import validate from "./loginValidate";
+import { login } from "../../actions/auth";
+import Register from "./Register";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+      {/* <Link color="inherit" href="https://material-ui.com/">
         Your Website
-      </Link>{" "}
+      </Link>{" "} */}
       {new Date().getFullYear()}
       {"."}
     </Typography>
@@ -176,8 +54,53 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function SignIn() {
+function Login({ isAuthenticated, login }) {
+  // const theme = useTheme();
   const classes = useStyles();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { email, password } = formData;
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    isSubmitting && setErrors(validate(formData));
+  };
+
+  const onSubmit = async e => {
+    if (e) {
+      e.preventDefault();
+      console.log("hi");
+      setErrors(validate(formData));
+      setIsSubmitting(true);
+    }
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
+      setIsSubmitting(false);
+      login({ email, password });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [errors]);
+
+  useEffect(() => {
+    if (isSubmitting) {
+      setIsSubmitting(false);
+      window.scrollTo(0, 0);
+    }
+  }, [isSubmitting]);
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -187,7 +110,10 @@ export default function SignIn() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Log in
+        </Typography>
+        <Typography component="h1" variant="caption">
+          Log in below to access you recipes!{" "}
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
@@ -200,7 +126,20 @@ export default function SignIn() {
             name="email"
             autoComplete="email"
             autoFocus
+            onChange={e => onChange(e)}
           />
+          {errors.email && (
+            <p
+              style={{
+                fontSize: ".7rem",
+                float: "left",
+                margin: "0px",
+                padding: "0px"
+              }}
+            >
+              {errors.email}
+            </p>
+          )}
           <TextField
             variant="outlined"
             margin="normal"
@@ -211,29 +150,43 @@ export default function SignIn() {
             type="password"
             id="password"
             autoComplete="current-password"
+            onChange={e => onChange(e)}
           />
-          <FormControlLabel
+          {errors.password && (
+            <p
+              style={{
+                fontSize: ".7rem",
+                float: "left",
+                margin: "0px",
+                padding: "0px"
+              }}
+            >
+              {errors.password}
+            </p>
+          )}
+          {/* <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
             label="Remember me"
-          />
+          /> */}
           <Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={e => onSubmit(e)}
           >
             Sign In
           </Button>
           <Grid container>
-            <Grid item xs>
+            {/* <Grid item xs>
               <Link href="#" variant="body2">
                 Forgot password?
               </Link>
-            </Grid>
+            </Grid> */}
             <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
+              <Link component={rrLink} variant="body2" to="/register">
+                {"Don't have an account? Register here!"}
               </Link>
             </Grid>
           </Grid>
@@ -245,3 +198,14 @@ export default function SignIn() {
     </Container>
   );
 }
+
+Login.propTypes = {
+  isAuthenticated: PropTypes.bool,
+  login: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
