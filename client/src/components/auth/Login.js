59f46c42a74f95df835749aad09f45cc91157d53
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-03-05 17:33:46 -0600

    redux form validation login integration

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
@@ -53,6 +54,17 @@ const useStyles = makeStyles(theme => ({
 
 function Login({ isAuthenticated, login }) {
   // const theme = useTheme();
+
+  const { register, handleSubmit, errors } = useForm();
+
+  const [emailError, setEmailError] = useState("");
+
+  const [isEmailError, setIsEmailError] = useState(false);
+
+  const [passwordError, setPasswordError] = useState("");
+
+  const [isPasswordError, setIsPasswordError] = useState(false);
+
   const classes = useStyles();
 
   const [formData, setFormData] = useState({
@@ -67,17 +79,31 @@ function Login({ isAuthenticated, login }) {
   };
 
   const onSubmit = async e => {
-    if (e) {
-      e.preventDefault();
-      window.scrollTo(0, 0);
-      login({ email, password });
-    }
+    window.scrollTo(0, 0);
+    login({ email, password });
   };
+
+  useEffect(() => {
+    console.log("error");
+    if (errors.email) {
+      setEmailError(errors.email.message);
+      setIsEmailError(true);
+    } else {
+      setEmailError("");
+      setIsEmailError(false);
+    }
+    if (errors.password) {
+      setPasswordError(errors.password.message);
+      setIsPasswordError(true);
+    } else {
+      setPasswordError("");
+      setIsPasswordError(false);
+    }
+  }, [errors.email, errors.password]);
 
   if (isAuthenticated) {
     return <Redirect to="/dashboard" />;
   }
-
   return (
     <Container component="main" maxWidth="xs">
       <div className={classes.paper}>
@@ -90,7 +116,11 @@ function Login({ isAuthenticated, login }) {
         <Typography component="h1" variant="caption">
           Log in below to access you recipes!{" "}
         </Typography>
-        <form className={classes.form} noValidate>
+        <form
+          className={classes.form}
+          noValidate
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <TextField
             variant="outlined"
             margin="normal"
@@ -101,6 +131,15 @@ function Login({ isAuthenticated, login }) {
             name="email"
             autoComplete="email"
             autoFocus
+            helperText={emailError}
+            error={isEmailError}
+            inputRef={register({
+              required: { value: true, message: "Email address is required" },
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Invalid email address"
+              }
+            })}
             onChange={e => onChange(e)}
           />
           <TextField
@@ -113,6 +152,15 @@ function Login({ isAuthenticated, login }) {
             type="password"
             id="password"
             autoComplete="current-password"
+            helperText={passwordError}
+            error={isPasswordError}
+            inputRef={register({
+              required: { value: true, message: "Password is required" },
+              minLength: {
+                value: 6,
+                message: "Password must be atleast 6 characters long"
+              }
+            })}
             onChange={e => onChange(e)}
           />
           {/* <FormControlLabel
@@ -125,7 +173,7 @@ function Login({ isAuthenticated, login }) {
             variant="contained"
             color="primary"
             className={classes.submit}
-            onClick={e => onSubmit(e)}
+            // onClick={e => onSubmit(e)}
           >
             Log In
           </Button>
