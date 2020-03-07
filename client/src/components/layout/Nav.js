afedcaa42475069f96824595e07d25161f4cf85e
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-03-06 22:10:57 -0600

    list items moved to Nav.js for access to redux actions

--- client/src/components/layout/Nav.js
+++ client/src/components/layout/Nav.js
@@ -11,11 +11,23 @@ import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 // import Badge from "@material-ui/core/Badge";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 // import NotificationsIcon from "@material-ui/icons/Notifications";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import PeopleIcon from "@material-ui/icons/People";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import SettingsIcon from "@material-ui/icons/Settings";
+import NoMeetingRoomIcon from "@material-ui/icons/NoMeetingRoom";
+import MeetingRoomIcon from "@material-ui/icons/MeetingRoom";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
 
-import { mainListItems, secondaryListItems, guestListItems } from "./listItems";
+import ListItemLink from "../routing/ListItemLink";
+import { logout } from "../../actions/auth";
 
 const drawerWidth = 240;
 
@@ -76,7 +88,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const Nav = ({ auth: { isAuthenticated, loading, user } }) => {
+const Nav = ({ auth: { isAuthenticated, loading, user }, logout }) => {
   const classes = useStyles();
 
   const [open, setOpen] = React.useState(false);
@@ -137,13 +149,65 @@ const Nav = ({ auth: { isAuthenticated, loading, user } }) => {
         <Divider />
         {isAuthenticated ? (
           <Fragment>
-            <List>{mainListItems}</List>
+            <List>
+              <ListItemLink to="/" primary="Home" icon={<HomeIcon />} />
+              <ListItem button>
+                <ListItemIcon>
+                  <SearchIcon />
+                </ListItemIcon>
+                <ListItemText primary="Search" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <FavoriteIcon />
+                </ListItemIcon>
+                <ListItemText primary="Favorites" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <PeopleIcon />
+                </ListItemIcon>
+                <ListItemText primary="Users" />
+              </ListItem>
+            </List>
             <Divider />
-            <List>{secondaryListItems}</List>
+            <List>
+              <ListItem button>
+                <ListItemIcon>
+                  <SettingsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Account Settings" />
+              </ListItem>
+              <ListItem button onClick={logout}>
+                <ListItemIcon>
+                  <NoMeetingRoomIcon />
+                </ListItemIcon>
+                <ListItemText primary="Log out" />
+              </ListItem>
+            </List>
           </Fragment>
         ) : (
           <Fragment>
-            <List>{guestListItems}</List>
+            <List>
+              <ListItem button>
+                <ListItemIcon>
+                  <SearchIcon />
+                </ListItemIcon>
+                <ListItemText primary="Search" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <MeetingRoomIcon />
+                </ListItemIcon>
+                <ListItemText primary="Log in" />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <VpnKeyIcon />
+                </ListItemIcon>
+                <ListItemText primary="Register" />
+              </ListItem>
+            </List>
           </Fragment>
         )}
       </Drawer>
@@ -152,11 +216,12 @@ const Nav = ({ auth: { isAuthenticated, loading, user } }) => {
 };
 
 Nav.propTypes = {
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps)(Nav);
+export default connect(mapStateToProps, { logout })(Nav);
