e9cdbbf4456a1f2c8faf9797cc7361c75ad43da8
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-03-06 22:50:19 -0600

    browse page set up

--- client/src/components/layout/Browse.js
+++ client/src/components/layout/Browse.js
@@ -1,12 +1,78 @@
-import React, { Fragment } from "react";
+import React from "react";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Link from "@material-ui/core/Link";
 
 import Recipes from "../recipes/Recipes";
+import Nav from "../layout/Nav";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto"
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column"
+  },
+  centerFlexibleItem: {
+    alignSelf: "center"
+  },
+  fixedHeight: {
+    height: 260
+  },
+  appBarSpacer: theme.mixins.toolbar
+}));
 
 const Browse = () => {
+  const classes = useStyles();
+
   return (
-    <Fragment>
-      <Recipes />
-    </Fragment>
+    <div className={classes.root}>
+      <Nav />
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <Recipes />
+              </Paper>
+            </Grid>
+          </Grid>
+          <Box pt={4}>
+            <Copyright />
+          </Box>
+        </Container>
+      </main>
+    </div>
   );
 };
 
