c203c364bb97681e3f4f0961bc2e567d95cd5343
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-04-15 22:49:54 -0500

    try block doesnt appear to be reached, the connect to mongoose failing might be why so comment it out  but log process.env in catch block incase it still does get reached

--- config/db.js
+++ config/db.js
@@ -6,15 +6,16 @@ const connectDB = async () => {
   try {
     console.log("in config folder...");
     console.log(process.env);
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
+    // await mongoose.connect(db, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    //   useFindAndModify: false
+    // });
     console.log("MongoDB Connected...");
   } catch (err) {
     console.log("failing");
+    console.log(process.env);
     console.log(err.message);
     process.exit(1);
   }
