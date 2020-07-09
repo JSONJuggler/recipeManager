3a32b5008b36491c3e34a22d2b827011cca0c66f
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-07-08 20:36:22 -0500

    responsive fonts and color change

--- themes/theme.js
+++ themes/theme.js
@@ -1,22 +1,24 @@
-import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
 
 // Create a theme instance.
-const theme = createMuiTheme({
+let theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#333333"
+      main: "#fff",
     },
     secondary: {
-      main: "#999999"
+      main: "#999999",
     },
     error: {
-      main: red.A400
+      main: red.A400,
     },
     background: {
-      default: "#999999"
-    }
-  }
+      default: "#999999",
+    },
+  },
 });
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
