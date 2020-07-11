1a95f6cf523ea93b0ff4372dcbe014e61bc64a9d
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-07-11 00:37:13 -0500

    functioning browse page using nextjs and strapi

--- components/Browse.js
+++ components/Browse.js
@@ -1,63 +1,115 @@
 import React, { Fragment } from "react";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import CreateIcon from "@material-ui/icons/Create";
+import Typography from "@material-ui/core/Typography";
 
-import Nav from "../layout/Nav";
-import Recipes from "../recipes/Recipes";
+//import Recipes from "../recipes/Recipes";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-    height: "100vh"
+    height: "100vh",
   },
   content: {
     flexGrow: 1,
-    overflow: "auto"
+    overflow: "auto",
   },
   spacer: {
     paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4)
+    paddingBottom: theme.spacing(4),
   },
-  appBarSpacer: theme.mixins.toolbar
+  appBarSpacer: theme.mixins.toolbar,
+  listRoot: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    // overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: { width: "100%", maxHeight: "75vh" },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)",
+  },
 }));
 
-const Browse = () => {
+const Browse = ({ recipes }) => {
   const classes = useStyles();
+
+  const xs = useMediaQuery("(min-width:0px)");
+  const sm = useMediaQuery("(min-width:600px)");
+  const md = useMediaQuery("(min-width:960px)");
+  const lg = useMediaQuery("(min-width:1280px)");
+  const xl = useMediaQuery("(min-width:1920px)");
+
+  const getCol = () => {
+    if (xl) {
+      return 5;
+    } else if (lg) {
+      return 4;
+    } else if (md) {
+      return 3;
+    } else if (sm) {
+      return 2;
+    } else if (xs) {
+      return 1;
+    }
+  };
 
   return (
     <div className={classes.root}>
       <Fragment>
-        <Nav />
         <main className={classes.content}>
           <div className={classes.appBarSpacer} />
-          {/* <Container maxWidth="lg"> */}
           <Container className={classes.spacer}>
-            {/* <Grid container spacing={4}> */}
-
-            <Recipes />
-
-            {/* </Grid> */}
-            <Box pt={4}>
-              <Copyright />
-            </Box>
-            {/* </Container> */}
+            <div className={classes.listRoot}>
+              <GridList
+                cellHeight={240}
+                className={classes.gridList}
+                cols={getCol()}
+              >
+                <GridListTile
+                  key="Subheader"
+                  cols={getCol()}
+                  style={{ height: "auto" }}
+                >
+                  <ListSubheader component="div">Recipes</ListSubheader>
+                </GridListTile>
+                {recipes &&
+                  recipes.map((recipeData) => (
+                    <GridListTile
+                      key={recipeData._id}
+                      cols={recipeData.cols || 1}
+                    >
+                      <img
+                        src="https://source.unsplash.com/random"
+                        alt="tile.title"
+                      />
+                      <GridListTileBar
+                        title={recipeData.name}
+                        subtitle={recipeData.description}
+                        actionIcon={
+                          <IconButton
+                            aria-label={"Favorite"}
+                            className={classes.icon}
+                          >
+                            <FavoriteIcon />
+                          </IconButton>
+                        }
+                      />
+                    </GridListTile>
+                  ))}
+                {!recipes && <Typography>No Recipes Found!</Typography>}
+              </GridList>
+            </div>
           </Container>
         </main>
       </Fragment>
---
+++ pages/browse.js
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Grow from "@material-ui/core/Grow";
+import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+import Browse from "components/Browse";
+import Footer from "components/Footer";
+
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    height: "100%",
+    flexGrow: 1,
+  },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  loadingContent: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  date: {
+    visibility: "hidden",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  progress: {
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
+
+const BrowseRecipes = ({ recipes }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Container className={classes.content} maxWidth="lg">
+        <Browse recipes={recipes} />
+      </Container>
+    </div>
+  );
+};
+
+export async function getServerSideProps() {
+  const res = await axios.get(
+    process.env.NEXT_PUBLIC_STRAPI_API_URL + "/recipes"
+  );
+
+  const recipes = res.data;
+
+  return {
+    props: { recipes },
+  };
+}
+
+// Index.propTypes = {
+//   rollbar: PropTypes.object.isRequired
+// };
+
+const mapStateToProps = (state) => ({
+  translations: state.translations,
+});
+
+export default connect(mapStateToProps)(BrowseRecipes);
